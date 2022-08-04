6259d47129d3325c8e4289728bf153779bc31ef1
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-04 14:20:49 -0400

    implement delete and update functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
+from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 from django.http import HttpResponse
 
 # Add the Cat class & list and view function below the imports
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
-  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', { 'finches': finches })
 
 
 def home(request):
@@ -25,5 +17,23 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-def finches_index(request):
-  return render(request, 'finches/index.html', { 'finches': finches })
+
+def home(request):
+  return render(request, 'home.html')
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description', 'age']
+  success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
