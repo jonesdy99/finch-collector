c78b225f9f032a02fdfe713ee6af78b324014505
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-03 21:42:41 -0400

    implement filler images and data sets

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Add the Cat class & list and view function below the imports
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
+def home(request):
+  return HttpResponse('<h1>Welcome to the website</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
